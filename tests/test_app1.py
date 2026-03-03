8bf95a26a657b8102997d7569cba96fcbafc6595
h1zzi43 <avedeneev20@gmail.com>
2026-03-03 17:00:20 +0300

    новый тест

---
+++ tests/test_app1.py
@@ -0,0 +1,11 @@
+def test_example():
+    """Пример теста1"""
+    assert True
+
+def test_addition():
+    """Еще один тест"""
+    assert 1 + 1 == 2
+
+def test_string():
+    """Тест строки"""
+    assert "hello" == "hello"
