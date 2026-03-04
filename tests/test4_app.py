d5e0498cc207bdd4a8ccf4e88daa24c86382d408
h1zzi43 <avedeneev20@gmail.com>
2026-03-04 10:47:04 +0300

    4 тест

---
+++ tests/test4_app.py
@@ -0,0 +1,11 @@
+def test_example():
+    """Пример теста"""
+    assert True
+
+def test_addition():
+    """Еще один тест"""
+    assert 2 + 3 == 5
+
+def test_string():
+    """Тест строки"""
+    assert "hello" == "hello"
